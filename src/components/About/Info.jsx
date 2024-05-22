0f1f05a9187e98fe6a5466e084cedf0abbc6b095
Adi <adityachimote@gmail.com>
2024-05-22 20:06:59 +0530

    "minor changes"

--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -21,7 +21,7 @@ function Info() {
       <div className="about__box">
         <h3 className="about__title">
           <img className="about__icon" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABsAAAAbCAYAAACN1PRVAAAAAXNSR0IArs4c6QAAAoJJREFUSEuNVoGR2zAMAyepM0mTSZpM0ngSZ5PkJ4k3YQ2JskhLtuu7/88rFCGCIGQRAFp/Ac1nhCd9bSv+c1pqFtq9ceVkV83XyWxLe5hcP31s8wBgEOC3Cq5QmQH9ATAD+NQkEcof7hgsR/5dEj1LsqayvDAL8FJgPGJTDnrASiZArqlLltQqmW3hympds1jlzWIaxtrKclImebvoJwRjVMZ6TIL9WTaV6gn4CvGmgx7YAMXXgFjBQ6CfXQXWE5EJHpB/ZwgeUPlU7QKORoEsDVLB2yqLlJxoPu3NQO+lf4MKZigunstQmQB3TX1KXbipqewQpx0VAhZmngKMnhUPziAGvwA8ekPan6F4HIHeVTBRpQAuJU9QY5JG3kdFrbNzYgy9OR0g+Jo1rbkyjTnbPUk925WExjqL2nMA73IWQ7FQ1VTpOn8FjL0iIEvnDwNSdUf2k301HIEANILkOCmH4FZGtUQWsJKeQKQgPIeU5i+3eXLfLGtJVsou/69glaL/6l4Gs02LWGYNYDkH5+NaaFk+c5BrZZ2m1H4HNU5Jjatr6KyCy5bGncpiNc3MNcXKBCh7Xx66yQ5YrSDSuCfDrUWo5MrqISqYO9hkDtLvWZN0d8H1LCn5pckns6qLtr3NcDW7yK70O/Q2akwjxBwfgjhvTEY8LH7IOSlg9fhtb9q3ES+0PKNhdIIR72V2OGz8r83gjWZLXObd9rUpZ0VkZ308je2LlaE4sDweXmnJaVIED+Ev0HC9JBobKR+rzo2H3X9NfHoR4o3xWSt2hw5ldrzOV0yllZ4Gz7R8r+U+Hus7S6Qm0OiUGZi2W3ijyxPr6jhOprHD5Z74anwH7NDSgH82uRwvyGebRAAAAABJRU5ErkJggg==" alt="Support Icon" />
-          <br />Support
+          Support
         </h3>
         <br /><span className="about__subtitle">FrontEnd-Developer</span>
       </div>
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -7,7 +7,7 @@ function Skills() {
   return (
     <section className="skills section" id='skills'>
         <h2 className="skills__title__out">Skills</h2>
-        <h6 className="skills__title__out skills__title__h3">My technical lavel</h6>
+        <h6 className="skills__title__out skills__title__h3">My technical level</h6>
         
         
  <div className="skills__container container grid">
