ddefeff87b4ae854702f3c828d57c000f2d8d515
Adi <adityachimote@gmail.com>
2024-05-22 20:09:34 +0530

    "updated qualification"

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -20,7 +20,7 @@ function Qualification() {
           <div className="qualification__content">
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title"><i className="uil uil-location-arrow"></i>Degree: (B.SC) Bachlor of sciance</h3>
+                <h3 className="qualification__title"><i className="uil uil-location-arrow"></i>Degree: (B.SC) bachelor of science</h3>
                 <span className="qualification__subtitle"><img alt=""className="round" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABsAAAAbCAYAAACN1PRVAAAAAXNSR0IArs4c6QAAAnBJREFUSEuVlj9rVUEQxX9DsBIJVmIpaqMQbUQ7C8EqQkS/gKAQX2UjiCGikCjaWaigEj+ASASJTazSmkYFbbSx0srSwmJ9b/feu7O7s5uX29zL7uz8OXPm7BWGRwAH3csvF0t6M55MD3XrhulkKQ8Xoxib04Rr2WiX5rfAjIMLwGngCLAf+Av8RNjGyXtwvwMSaSi9pCoz657FsSSw6GBfnkl05L9eIazi5Idvh0azM2wFWwCeAgd36I6HXXCTEP+AkcDLrMCBAlZHrwHPE7LUIg5Z+2C90yUH9y0kcoJMKlovmpeDrzwr1PSxEcgzzfAcxlngWw7dNIy1GSgnwH3W/dO+HgE3zbmxS01XSza+AS734IrKaAb4Q8E6Y9jrc5ul5M8eR/jq9SIGkwVw6yUpaiOq66/bALcQHvbi1GfzwG9MDZeB4EDwRPbeAhcnW4EgIbHXHt8dVMCUmDhnmdT5+fvkhJN5ZR8EzsXZt93q5KOFod6xr98dHI2VBQ/vwM1bqm87rSUzqElf5RdgLg/2RHCjdmX1Cho4bIzFej7CGHxcAdaqEtWke308xsfujku8F/QzEuTAWN1/hcoKKDoSNSmephOJdgrY9j6za2INfIXhKXxPEyw5tgmc1+403Ic7bdyz2zjGxEz8ngW2hmBKQfo76SrwQq/ry7AgQr1dy8CKtleqn7i5LbDa968ZzEb8MXAj74PqWZHz9XBT69SNMso2LguslNddwkaDgWEY7wCXyjlKowhsOg+bbBX/IAPDcy9F8j6JY93f1RngELBX/V19xLHh6V1hSUoQg9EFQZo2Df1QWzWCtE/vYi606X/oteUi15coxQAAAABJRU5ErkJggg=="/>
                   University: Sant Gadge Baba Amaravati Unviersity
                 </span>
