20387772c0077319bcb152b57a4abfa62bc92ef9
Adi <adityachimote@gmail.com>
2024-10-10 17:12:56 +0530

    "make some edits "

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -67,25 +67,22 @@ function Services() {
           <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
             <div className="services__modal-content">
               <i className="uil uil-times services__modal-close" onClick={() => toggleTab(0)}></i>
-              <h3 className="services__modal-title">React Developer</h3>
-              <p className="services__modal-description">Specializing in building modern web applications using React.</p>
+              <h3 className="services__modal-title">Back-end Developer</h3>
+              <p className="services__modal-description">Specializing in building modern web applications using Java , spring boot.</p>
               <ul className="services__modal-services grid">
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Component-Based Architecture.</p>
+                  <p className="services__modal-info">Authentication and Authorization</p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">State Management with Redux</p>
+                  <p className="services__modal-info">Implementing a microservices architecture to enhance scalability </p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Server-Side Rendering with Next.js</p>
+                  <p className="services__modal-info">Server-Side Rendering </p>
                 </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Design mockups of products for companies.</p>
-                </li>
+               
               </ul>
             </div>
           </div>
@@ -105,20 +102,17 @@ function Services() {
           <div className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
             <div className="services__modal-content">
               <i className="uil uil-times services__modal-close" onClick={() => toggleTab(0)}></i>
-              <h3 className="services__modal-title">JavaScript Developer</h3>
-              <p className="services__modal-description">Proficient in JavaScript and its modern frameworks.</p>
+              <h3 className="services__modal-title">Database Management</h3>
+              <p className="services__modal-description">Implementing efficient database management practices</p>
               <ul className="services__modal-services grid">
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Dynamic Web Applications.</p>
+                  <p className="services__modal-info">Proficient in using database languages such as SQL for relational databases </p>
                 </li>
+                
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Integration with RESTful APIs.</p>
-                </li>
-                <li className="services__modal-service">
-                  <i className="uil uil-check-circle services__modal-icon"></i>
-                  <p className="services__modal-info">Asynchronous Programming with Promises and Async/Await.</p>
+                  <p className="services__modal-info">Knowledge of performance tuning methods, such as query optimization and proper indexing</p>
                 </li>
                 <li className="services__modal-service">
                   <i className="uil uil-check-circle services__modal-icon"></i>
